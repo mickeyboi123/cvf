38e9dc7bfe7da1647c3b38cd17bb4ae7a832354c
mickeyboi123 <116971472+mickeyboi123@users.noreply.github.com>
2022-12-20 02:04:50 -0800

    Add files via upload

---
+++ mail.php
@@ -0,0 +1,15 @@
+<?php
+if (isset($_POST['submit']))
+
+    $errorEmpty = false;
+    $errorEmail = false;
+
+    else{
+        echo" <span class='form-success'>Fill in all fields!</span>";
+    }
+
+?>
+
+<script>
+    
+</script>
